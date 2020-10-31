2aa9e11e3d5199e37177c47041a27ecf8849645f
unknown <jayqwu@gmail.com>
2020-10-31 14:40:13 -0700

    first commit

--- Skywalkers2021/src/main/java/frc/robot/Robot.java
+++ Skywalkers2021/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -70,6 +71,7 @@ public class Robot extends TimedRobot {
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
+      System.out.println(Constants.RIGHT_MASTER_ID);  
     }
   }
 
